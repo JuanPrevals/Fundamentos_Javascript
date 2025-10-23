4dba31fdaaabbeffc3401853554414edf5f2a656
Juan Prevals <34948337+JuanPrevals@users.noreply.github.com>
2025-10-23 14:24:12 -0300

    updateee

--- 07_bucles_while_for/static/js/script.js
+++ 07_bucles_while_for/static/js/script.js
@@ -16,13 +16,17 @@ function counter() {
 
 
 function bucleFor() {
+    let resultado = '';
     for (let i = 10; i > 0; i--) {
         console.log(i);
         resultado += ` ${i}`;
     }
-    alert(Resultado)
+    alert(resultado); 
 }
 
+
+// 1️⃣ Bucle while: Imprimir del 1 a n (n es ingresado por teclado).
+// Escribe un programa que utilice un bucle while para imprimir los números del 1 al n con un solo Alert.
 
 function unoaN() {
     let n = parseInt(prompt("Ingresa un número:"));
@@ -35,6 +39,11 @@ function unoaN() {
 
     }
 }
+
+
+// 2️⃣ Bucle while: Cuenta regresiva del n al 1 (n es ingresado por teclado)
+// Crea un programa que muestre una cuenta regresiva desde n hasta 1 utilizando un bucle while.
+
 
 function Nauno() {
     let n = parseInt(prompt("Ingresa un número:"));
@@ -50,4 +59,8 @@ function Nauno() {
     }
 
     alert("Números del " + n + " al 1: " + resultado);
-} 
+}
+
+
+// 3️⃣ Bucle while: Sumar los n primeros números
+// Usando un bucle for, calcula la suma de los números n primeros números  muestra el resultado en la consola.
