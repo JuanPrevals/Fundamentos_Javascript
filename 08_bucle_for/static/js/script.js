e5b49e8e9ebd6362b9e3f011b03d4e1373a72329
Juan Prevals <34948337+JuanPrevals@users.noreply.github.com>
2025-11-03 08:46:47 -0300

    aa

--- 08_bucle_for/static/js/script.js
+++ 08_bucle_for/static/js/script.js
@@ -13,3 +13,57 @@ function tablaMultiplicar() {
     }
     alert(tablas);
 }
+
+function numeroPar() {
+    let num = parseInt(prompt("Ingresa un número"));
+    let resultado = `los numeros pares de ${num} son: `
+
+    for (let i = 1; i <= num; i++)
+        if (i % 2 == 0) {
+            resultado += `${i}`
+        }
+
+    alert(resultado);
+
+}
+
+function contarPar() {
+    let word = parseInt(prompt("Dime un numero"));
+    let cont = 0
+    for (let i = 0; i < word.length; i++) {
+        cont++
+    }
+    alert(`La palabra "${word} tiene ${count} letras`)
+}
+
+
+function calculoFactorial() {
+    let numero = parseInt(prompt("Ingrese su número"));
+    let factorial = 1;
+    let largo = "1";
+
+    for (let i = 2; i <= numero; i++) {
+        largo += ` * ${i}`;
+        factorial *= i;
+    }
+
+    alert(`El factorial de ${numero} es: ${largo} = ${factorial}`);
+
+}
+
+
+function numerosImpares()s {
+    let numero = parseInt(prompt("Ingresar números: "));
+    let respuesta = 0;
+    let largoImpar = "1"
+    for (let i = 2; i <= numero; i++) {
+        if (i % 2 != 0) {
+            respuesta += i
+            largoImpar += ` + ${i}`
+
+        }
+    }
+    respuesta += 1
+    alert(`Las sumas de ${largoImpar} = ${respuesta}`)
+
+}
